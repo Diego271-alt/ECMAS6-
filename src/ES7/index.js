679aa0ce2e34cbd73585d1e45dc8a50cf0d86a5c
Diego Romero <dr87152@gmail.com>
2020-06-12 12:21:34 -0500

    Caracteristicas de Ecma7

---
+++ src/ES7/index.js
@@ -0,0 +1,13 @@
+let numbers  = [1,2,3,4,7,8];
+
+if(numbers.includes(7)){
+    console.log('Si está');
+}else {
+    console.log(' no esta')
+}
+
+let base =4;
+
+let exponent= 3;
+
+let result= base ** exponent;
